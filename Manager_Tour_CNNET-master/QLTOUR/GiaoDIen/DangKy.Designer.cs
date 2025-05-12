9f85c0d19aa8f89c09d3a21930fd0a80ac459a60
Heulwen <52200028@student.tdtu.edu.vn>
2025-05-12 13:45:53 +0700

    Initial commit

---
+++ Manager_Tour_CNNET-master/QLTOUR/GiaoDIen/DangKy.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace QLTOUR.GiaoDIen
+{
+    partial class DangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DangKy));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_TenDN = new System.Windows.Forms.TextBox();
+            this.txt_MK = new System.Windows.Forms.TextBox();
+            this.txt_XNMK = new System.Windows.Forms.TextBox();
+            this.btn_DangKy = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.cbShowPass = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbb_MaNV = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(623, 238);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(623, 322);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(623, 412);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhập lại mật khẩu";
+            // 
+            // txt_TenDN
+            // 
+            this.txt_TenDN.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_TenDN.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_TenDN.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.txt_TenDN.Location = new System.Drawing.Point(627, 263);
+            this.txt_TenDN.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_TenDN.Name = "txt_TenDN";
+            this.txt_TenDN.Size = new System.Drawing.Size(424, 39);
+            this.txt_TenDN.TabIndex = 4;
+            // 
+            // txt_MK
+            // 
+            this.txt_MK.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_MK.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_MK.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.txt_MK.Location = new System.Drawing.Point(627, 348);
+            this.txt_MK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_MK.Name = "txt_MK";
+            this.txt_MK.Size = new System.Drawing.Size(424, 39);
+            this.txt_MK.TabIndex = 5;
+            this.txt_MK.UseSystemPasswordChar = true;
+            // 
+            // txt_XNMK
+            // 
+            this.txt_XNMK.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_XNMK.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_XNMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.txt_XNMK.Location = new System.Drawing.Point(627, 438);
+            this.txt_XNMK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_XNMK.Name = "txt_XNMK";
+            this.txt_XNMK.Size = new System.Drawing.Size(424, 39);
+            this.txt_XNMK.TabIndex = 6;
+            this.txt_XNMK.UseSystemPasswordChar = true;
+            // 
+            // btn_DangKy
+            // 
+            this.btn_DangKy.BackColor = System.Drawing.Color.Red;
+            this.btn_DangKy.FlatAppearance.BorderSize = 0;
+            this.btn_DangKy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_DangKy.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.btn_DangKy.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_DangKy.Location = new System.Drawing.Point(727, 523);
+            this.btn_DangKy.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_DangKy.Name = "btn_DangKy";
+            this.btn_DangKy.Size = new System.Drawing.Size(232, 49);
+            this.btn_DangKy.TabIndex = 7;
+            this.btn_DangKy.Text = "Đăng Ký";
+            this.btn_DangKy.UseVisualStyleBackColor = false;
+            this.btn_DangKy.Click += new System.EventHandler(this.btn_DangKy_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.Transparent;
+            this.exitButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("exitButton.BackgroundImage")));
+            this.exitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.exitButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitButton.FlatAppearance.BorderSize = 0;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.ForeColor = System.Drawing.Color.Transparent;
+            this.exitButton.Location = new System.Drawing.Point(1065, 13);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 63);
+            this.exitButton.TabIndex = 9;
+            this.exitButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // cbShowPass
+            // 
+            this.cbShowPass.AutoSize = true;
+            this.cbShowPass.BackColor = System.Drawing.Color.Transparent;
+            this.cbShowPass.FlatAppearance.BorderSize = 0;
+            this.cbShowPass.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbShowPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbShowPass.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cbShowPass.Location = new System.Drawing.Point(627, 487);
+            this.cbShowPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbShowPass.Name = "cbShowPass";
+            this.cbShowPass.Size = new System.Drawing.Size(163, 28);
+            this.cbShowPass.TabIndex = 11;
+            this.cbShowPass.Text = "Show Password";
+            this.cbShowPass.UseVisualStyleBackColor = false;
+            this.cbShowPass.CheckedChanged += new System.EventHandler(this.cbShowPass_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(760, 85);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 39);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Đăng Ký";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(59)))), ((int)(((byte)(127)))));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -7);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(451, 680);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(623, 153);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 24);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Nhân Viên";
+            // 
+            // cbb_MaNV
+            // 
+            this.cbb_MaNV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbb_MaNV.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbb_MaNV.FormattingEnabled = true;
+            this.cbb_MaNV.Location = new System.Drawing.Point(627, 178);
+            this.cbb_MaNV.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbb_MaNV.Name = "cbb_MaNV";
+            this.cbb_MaNV.Size = new System.Drawing.Size(423, 38);
+            this.cbb_MaNV.TabIndex = 15;
+            // 
+            // DangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(59)))), ((int)(((byte)(127)))));
+            this.ClientSize = new System.Drawing.Size(1153, 674);
+            this.Controls.Add(this.cbb_MaNV);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbShowPass);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.btn_DangKy);
+            this.Controls.Add(this.txt_XNMK);
+            this.Controls.Add(this.txt_MK);
+            this.Controls.Add(this.txt_TenDN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "DangKy";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DangKy";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DangKy_FormClosing);
+            this.Load += new System.EventHandler(this.DangKy_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_TenDN;
+        private System.Windows.Forms.TextBox txt_MK;
+        private System.Windows.Forms.TextBox txt_XNMK;
+        private System.Windows.Forms.Button btn_DangKy;
+          private System.Windows.Forms.Button exitButton;
+          private System.Windows.Forms.CheckBox cbShowPass;
+          private System.Windows.Forms.Label label4;
+          private System.Windows.Forms.PictureBox pictureBox1;
+          private System.ComponentModel.BackgroundWorker backgroundWorker1;
+          private System.Windows.Forms.Label label5;
+          private System.Windows.Forms.ComboBox cbb_MaNV;
+     }
+}
---
+++ Manager_Tour_CNNET-master/QLTOUR/GiaoDIen/DatTour.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace QLTOUR.GiaoDIen
+{
+     public partial class DatTour : Form
+     {
+          DBConnection db = new DBConnection();
+          public string MaNV { get; set; }
+          public DatTour(string manv)
+          {
+               InitializeComponent();
+               MaNV = manv;
+               TenNV();
+          }
+
+          void TenNV()
+          {
+               string sqltennv = "SELECT HOTEN FROM NhanVien WHERE MANV = '" + MaNV + "'";
+               SqlDataReader reader = db.ExcuteQuery(sqltennv);
+               if (reader.Read())
+               {
+                    string tennv = reader["HOTEN"].ToString();
+                    lb_tennv.Text = tennv;
+                    db.Close();
+               }
+               else
+               {
+                    lb_tennv.Text = "Không tìm thấy thông tin";
+                    db.Close();
+               }
+          }
+
+          private void QLTour_Load(object sender, EventArgs e)
+          {
+               HienDSTOUR();
+               
+          }
+          void clear()
+          {
+               txt_timtour.Clear();
+
+          }
+          void HienDSTOUR()
+          {
+               dgv_thongtintour.ReadOnly = true;
+               string chuoitruyvan = "SELECT * FROM LOAITOUR lt JOIN THONGTINTOUR ttt ON lt.MaLoaiTour = ttt.MaLoaiTour JOIN XUATPHATTOUR xpt ON ttt.MaXP = xpt.MaXP JOIN PHUONGTIENTOUR ptt ON ttt.MaPhuongTien = ptt.MaPhuongTien ";
+               DataTable dt = db.getDataTable(chuoitruyvan);
+               DataColumn[] key = new DataColumn[1];
+               key[0] = dt.Columns[0];
+
+               dgv_thongtintour.DataSource = dt;
+               dgv_thongtintour.Columns["MoTaTour"].Visible = false;
+               dgv_thongtintour.Columns["TGBatDau"].Visible = false;
+               dgv_thongtintour.Columns["TGKetThuc"].Visible = false;
+               dgv_thongtintour.Columns["MaLoaiTour"].Visible = false;
+               dgv_thongtintour.Columns["MaPhuongTien"].Visible = false;
+               dgv_thongtintour.Columns["MaPhuongTien1"].Visible = false;
+               dgv_thongtintour.Columns["MAXP"].Visible = false;
+               dgv_thongtintour.Columns["MAXP1"].Visible = false;
+               dgv_thongtintour.Columns["MaLoaiTour"].Visible = false;
+               dgv_thongtintour.Columns["MaLoaiTour1"].Visible = false;
+               dgv_thongtintour.Columns["TenLoaiTour"].Visible = false;
+
+               dgv_thongtintour.Columns["AnhTour"].Visible = false;
+
+
+          }
+
+          private void btn_tim_Click(object sender, EventArgs e)
+          {
+               string searchName = txt_timtour.Text.Trim();
+               LoadDataBySearch(searchName);
+          }
+
+          private void dgv_thongtintour_CellClick(object sender, DataGridViewCellEventArgs e)
+          {
+
+
+          }
+          private void LoadDataBySearch(string searchName)
+          {
+
+               string chuoitruyvan = "SELECT * FROM LOAITOUR lt JOIN THONGTINTOUR ttt ON lt.MaLoaiTour = ttt.MaLoaiTour JOIN XUATPHATTOUR xpt ON ttt.MaXP = xpt.MaXP JOIN PHUONGTIENTOUR ptt ON ttt.MaPhuongTien = ptt.MaPhuongTien WHERE TenTour COLLATE SQL_Latin1_General_CP1_CI_AI LIKE N'%' + '" + txt_timtour.Text.Trim() + "' + N'%'";
+
+               DataTable dt = db.getDataTable(chuoitruyvan);
+               dgv_thongtintour.DataSource = dt;
+          }
+
+          private void btn_caocap_Click(object sender, EventArgs e)
+          {
+               string chuoitruyvan = "SELECT * FROM LOAITOUR lt JOIN THONGTINTOUR ttt ON lt.MaLoaiTour = ttt.MaLoaiTour JOIN XUATPHATTOUR xpt ON ttt.MaXP = xpt.MaXP JOIN PHUONGTIENTOUR ptt ON ttt.MaPhuongTien = ptt.MaPhuongTien WHERE ttt.MaLoaiTour = 'TCC'";
+
+               DataTable dt = db.getDataTable(chuoitruyvan);
+
+               dgv_thongtintour.DataSource = dt;
+          }
+
+          private void btn_tieuchuan_Click(object sender, EventArgs e)
+          {
+               string chuoitruyvan = "SELECT * FROM LOAITOUR lt JOIN THONGTINTOUR ttt ON lt.MaLoaiTour = ttt.MaLoaiTour JOIN XUATPHATTOUR xpt ON ttt.MaXP = xpt.MaXP JOIN PHUONGTIENTOUR ptt ON ttt.MaPhuongTien = ptt.MaPhuongTien WHERE ttt.MaLoaiTour = 'TTC'";
+
+
+               DataTable dt = db.getDataTable(chuoitruyvan);
+
+               dgv_thongtintour.DataSource = dt;
+          }
+
+          private void btn_tietkiem_Click(object sender, EventArgs e)
+          {
+               string chuoitruyvan = "SELECT * FROM LOAITOUR lt JOIN THONGTINTOUR ttt ON lt.MaLoaiTour = ttt.MaLoaiTour JOIN XUATPHATTOUR xpt ON ttt.MaXP = xpt.MaXP JOIN PHUONGTIENTOUR ptt ON ttt.MaPhuongTien = ptt.MaPhuongTien WHERE ttt.MaLoaiTour = 'TTK'";
+
+
+               DataTable dt = db.getDataTable(chuoitruyvan);
+
+               dgv_thongtintour.DataSource = dt;
+          }
+
+          private void dgv_thongtintour_CellContentClick(object sender, DataGridViewCellEventArgs e)
+          {
+               dgv_thongtintour.ReadOnly = true;
+               if (e.RowIndex >= 0)
+               {
+                    DataGridViewRow row = dgv_thongtintour.Rows[e.RowIndex];
+
+                    btn_datve.Visible = true;
+                    lb_matour.Text = row.Cells["MaTour"].Value.ToString();
+                    lb_matour.Visible = true;
+                    lb_tenloaitour.Text = row.Cells["TenLoaiTour"].Value.ToString();
+                    lb_tenloaitour.Visible = true;
+                    lb_tentour.Text = row.Cells["TenTour"].Value.ToString();
+                    lb_tentour.Visible = true;
+                    lb_giatour.Text = ((decimal)row.Cells["GiaTour"].Value).ToString("C0");
+                    lb_giatour.Visible = true;
+                    //lb_motatour.Text = row.Cells["MoTaTour"].Value.ToString();
+                    //lb_motatour.Visible = true;
+                    richboxDescription.Text = row.Cells["MoTaTour"].Value.ToString();
+                    richboxDescription.Visible = true;
+                    lb_valuexp.Text = row.Cells["MaXP"].Value.ToString();
+                    lb_valuexp.Visible = true;
+                    lb_xp.Visible = true;
+                    lb_valuephuongtien.Text = row.Cells["MaPhuongTien"].Value.ToString();
+                    lb_valuephuongtien.Visible = true;
+                    lb_phuongtien.Visible = true;
+                    lb_valueslve.Text = row.Cells["SLVeConLai"].Value.ToString();
+                    lb_valueslve.Visible = true;
+                    lb_slve.Visible = true;
+
+                    lb_thoigian.Visible = true;
+                    lb_tgbatdau.Text = row.Cells["TGBatDau"].Value.ToString();
+                    lb_tgbatdau.Visible = true;
+                    lb_tgketthuc.Text = row.Cells["TGKetThuc"].Value.ToString();
+                    lb_tgketthuc.Visible = true;
+
+                    lbIDTour.Visible = true;
+                    lbDescription.Visible = true;
+                    lbPrice.Visible = true;
+                    lbType.Visible = true;
+                    string anhtour = row.Cells["AnhTour"].Value.ToString();
+
+
+                    ptb_anhtour.Visible = true;
+                    // Đặt chỉ mục của hình ảnh cần sử dụng từ ImageList
+                    int imageIndex = imageList1.Images.IndexOfKey(anhtour);
+
+                    // Kiểm tra xem chỉ mục có hợp lệ không
+                    if (imageIndex != -1)
+                    {
+                         // Gán hình ảnh từ ImageList cho PictureBox
+                         ptb_anhtour.Image = imageList1.Images[imageIndex];
+                    }
+                    else
+                    {
+                         // Xử lý khi hình ảnh không tồn tại trong ImageList
+                        ptb_anhtour.Image = imageList1.Images[1];
+                    }
+
+                    //dt_ngaysinh.Value = Convert.ToDateTime(row.Cells["NgaySinh"].Value);
+
+               }
+          }
+
+          private void btn_datve_Click(object sender, EventArgs e)
+          {
+               string matour = lb_matour.Text;
+               Ve datve = new Ve(matour, MaNV);
+               this.Hide();
+               datve.ShowDialog();
+          }
+
+          private void comboBoxTourType_SelectedIndexChanged(object sender, EventArgs e)
+          {
+              FilterTours();
+          }
+
+          private void comboBoxBudget_SelectedIndexChanged(object sender, EventArgs e)
+          {
+              FilterTours();
+          }
+ 
+          private void comboBoxTransport_SelectedIndexChanged(object sender, EventArgs e)
+          {
+              FilterTours();
+          }
+
+
+        private void FilterTours()
+          {
+              StringBuilder query = new StringBuilder();
+              query.Append("SELECT * FROM LOAITOUR lt ");
+              query.Append("JOIN THONGTINTOUR ttt ON lt.MaLoaiTour = ttt.MaLoaiTour ");
+              query.Append("JOIN XUATPHATTOUR xpt ON ttt.MaXP = xpt.MaXP ");
+              query.Append("JOIN PHUONGTIENTOUR ptt ON ttt.MaPhuongTien = ptt.MaPhuongTien ");
+
+              List<string> conditions = new List<string>();
+
+              // Loại tour
+              if (comboBoxTourType.SelectedItem != null)
+              {
+                  string loai = comboBoxTourType.SelectedItem.ToString();
+                  if (loai == "Cao Cấp") conditions.Add("ttt.MaLoaiTour = 'TCC'");
+                  else if (loai == "Tiêu Chuẩn") conditions.Add("ttt.MaLoaiTour = 'TTC'");
+                  else if (loai == "Tiết Kiệm") conditions.Add("ttt.MaLoaiTour = 'TTK'");
+              }
+
+              // Mức giá
+              if (comboBoxBudget.SelectedItem != null)
+              {
+                  string budget = comboBoxBudget.SelectedItem.ToString();
+                  if (budget == "Dưới $200") conditions.Add("ttt.GiaTour < 200");
+                  else if (budget == "Từ $200 đến $400") conditions.Add("ttt.GiaTour >= 200 AND ttt.GiaTour <= 400");
+                  else if (budget == "Từ $400 đến $800") conditions.Add("ttt.GiaTour >= 400 AND ttt.GiaTour <= 800");
+                  else if (budget == "Trên $800") conditions.Add("ttt.GiaTour > 800");
+              }
+
+              // Phương tiện
+              if (comboBoxTransport.SelectedItem != null)
+              {
+                  string transport = comboBoxTransport.SelectedItem.ToString();
+                  conditions.Add($"ptt.TenPhuongTien = N'{transport}'");
+              }
+
+              if (conditions.Count > 0)
+              {
+                  query.Append(" WHERE ");
+                  query.Append(string.Join(" AND ", conditions));
+              }
+
+              DataTable dt = db.getDataTable(query.ToString());
+              dgv_thongtintour.DataSource = dt;
+          }
+
+
+        private void btn_all_Click(object sender, EventArgs e)
+          {
+              HienDSTOUR();
+          }
+          //private void btn_TrangChu_Click(object sender, EventArgs e)
+          //{
+          //     this.Hide();
+          //     MAIN_QL main = new MAIN_QL(MaNV);
+          //     main.ShowDialog();
+          //}
+     }
+}
